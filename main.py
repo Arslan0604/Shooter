cee899eac3ef714fb8fc252e39841bba5dcfdcf1
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-19 18:45:25 +0500

    ars3

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 import sys
+from random import randint
+
+clock = pygame.time.Clock()
+
 
 pygame.init()
 
-pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 600))
 
 pygame.display.set_caption("My Pygame")
+
 
 while True:
     for event in pygame.event.get():
@@ -13,6 +18,10 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
 
+    screen.fill((randint(0, 255), randint(0, 255), randint(0, 255)))
+    pygame.display.update()
+
+    clock.tick(1)
 
 
 
