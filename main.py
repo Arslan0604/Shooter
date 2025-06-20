63ececdf3c3f2c9ddeadd39cb90fd31cc11cd787
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-20 12:22:15 +0500

    ars5

--- main.py
+++ main.py
@@ -27,12 +27,12 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP and rect_y >= STEP:
-                rect_y -= rect_y - STEP
-            if event.key == pygame.K_DOWN and rect_y <= screen_height - STEP:
-                rect_y += rect_y + STEP
-            if event.key == pygame.K_LEFT:
+                rect_y -= STEP
+            if event.key == pygame.K_DOWN and rect_y <= screen_height - rect_height - STEP:
+                rect_y += STEP
+            if event.key == pygame.K_LEFT and rect_x >= STEP:
                 rect_x -= STEP
-            if event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT and rect_x <= screen_width - rect_width - STEP:
                 rect_x += STEP
 
     screen.fill(fill_color)
