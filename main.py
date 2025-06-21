7ca51e68d764cf03b62ee47cdd8a7d52661c060c
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-21 09:56:31 +0500

    ars1

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import pygame
 
+
+
+pygame.init()
+
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+
+pygame.display.set_caption("Awesome Shooter Game")
 
 
 
