dcae8faacabd955caa3205eb30a10139177e9959
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-22 10:38:37 +0500

    ars5

--- main.py
+++ main.py
@@ -7,21 +7,39 @@ pygame.init()
 screen_width, screen_height = 800, 600
 screen_fill_color = (32, 52, 71)
 screen = pygame.display.set_mode((screen_width, screen_height))
+
 pygame.display.set_caption("Awesome Shooter Game")
+
+FIGHTER_STEP = 0.1 # step of moving of ship #
+# function of moving of this ship #
 fighter_image = pygame.image.load('images_game/fighter.png')
 fighter_width, fighter_height = fighter_image.get_size()
 fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_height
-FIGHTER_STEP = 20
+fighter_is_moving_left, fighter_is_moving_right = False, False
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+            # moving to left and right that shown below
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and fighter_x >= FIGHTER_STEP:
-                fighter_x -= FIGHTER_STEP
-            if event.key == pygame.K_RIGHT and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
-                fighter_x += FIGHTER_STEP
+            if event.key == pygame.K_LEFT:
+                fighter_is_moving_left = True
+
+            if event.key == pygame.K_RIGHT:
+                fighter_is_moving_right = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                fighter_is_moving_left = False
+            if event.key == pygame.K_RIGHT:
+                fighter_is_moving_right = False
+
+    if fighter_is_moving_left and fighter_x >= FIGHTER_STEP:
+        fighter_x -= FIGHTER_STEP
+    if fighter_is_moving_right and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
+        fighter_x += FIGHTER_STEP
 
 
     screen.fill(screen_fill_color)
