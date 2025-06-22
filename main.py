fd9cad8f35af1f9b10eba1d7c4a9f22a524f9e44
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-22 09:28:00 +0500

    ars2

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import pygame
+import sys
 
 
 
 pygame.init()
-
 screen_width, screen_height = 800, 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-
 pygame.display.set_caption("Awesome Shooter Game")
 
-fighter_image = pygame.image.load()
+fighter_image = pygame.image.load('images_game/starship.png')
 
-# Arslan you have to dome smarter:
-# Now start make images:
+fighter_width, fighter_height = fighter_image.get_size()
 
+fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_height
+
+
+while True:
+    for event in pygame.event.get():
 
 
 
