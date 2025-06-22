e8348e25a3ae01ca9e59625883c4a84555ab32fa
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-22 09:33:23 +0500

    ars2

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 
 pygame.init()
 screen_width, screen_height = 800, 600
+screen_fill_color = (32, 52, 71)
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Awesome Shooter Game")
@@ -18,6 +19,11 @@ fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fig
 
 while True:
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill(screen_fill_color)
+    screen.blit(fighter_image, (fighter_x, fighter_y))
 
 
 
